1cc598142b352cfd49fceb1e5da72e2508ca1a08
Luis Evilla <luisfelipe588@hotmail.com>
2020-06-15 22:52:58 -0500

    fetching data to homeScreen from server

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import data from '../data';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import data from '../data';
 
 function ProductScreen(props) {
-    console.log(props.match.params.id);
+
     const product = data.products.find(x => x._id === props.match.params.id);
+
     return (
         <div>
             <div className="back-to-results">
